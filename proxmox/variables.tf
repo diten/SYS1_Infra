68b8ce66ddeb1a66ec22d6b546c0614103d86c6d
diten <diego.diez@gmail.com>
2025-12-16 20:05:53 +0100

    vars fixed (#2)

---
+++ proxmox/variables.tf
@@ -0,0 +1,24 @@
+variable "prox_api_url" {
+   type = string
+   default = "https://192.168.18.20:8006/api2/json"
+}
+
+variable "prox_api_token_id" {
+  type = string
+  default = ""
+}
+
+variable "prox_api_token_secret" {
+  type = string
+  default = ""
+}
+
+variable "ci_user" {
+  type = string
+  default = ""
+}
+
+variable "ci_password" {
+  type = string
+  default = ""
+}
